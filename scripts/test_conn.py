100632a9b492b493f16c3ad436b6c78c10f7cee7
Camelia Mazouz <145675762+mica1505@users.noreply.github.com>
2026-03-18 11:32:46 +0100

    fix: sql coms

--- scripts/test_conn.py
+++ scripts/test_conn.py
@@ -1,42 +1,37 @@
 import snowflake.connector
 import os
 
-def deploy_infrastructure():
-    # Récupération des variables injectées par le YAML
+def deploy():
+    # Récupération des variables Git
     db = os.getenv('SNOWFLAKE_DATABASE').upper()
-    warehouse = os.getenv('SNOWFLAKE_WAREHOUSE').upper()
-    
-    ctx = snowflake.connector.connect(
+    wh = os.getenv('SNOWFLAKE_WAREHOUSE').upper()
+
+    conn = snowflake.connector.connect(
         user=os.getenv('SNOWFLAKE_USERNAME'),
         password=os.getenv('SNOWFLAKE_PASSWORD'),
         account=os.getenv('SNOWFLAKE_ACCOUNT'),
-        role='ACCOUNTADMIN' 
+        role='ACCOUNTADMIN'
     )
-    
+
     try:
-        cs = ctx.cursor()
+        cur = conn.cursor()
+        # On définit le contexte d'abord
+        cur.execute(f"CREATE WAREHOUSE IF NOT EXISTS {wh} WITH WAREHOUSE_SIZE='XSMALL'")
+        cur.execute(f"USE WAREHOUSE {wh}")
+        cur.execute(f"CREATE DATABASE IF NOT EXISTS {db}")
+        cur.execute(f"USE DATABASE {db}")
         
-        # Liste des commandes utilisant les f-strings pour injecter tes variables Git
-        commands = [
-            f"CREATE WAREHOUSE IF NOT EXISTS {warehouse} WITH WAREHOUSE_SIZE='XSMALL'",
-            f"USE WAREHOUSE {warehouse}",
-            f"CREATE DATABASE IF NOT EXISTS {db}",
-            f"USE DATABASE {db}",
-            "CREATE SCHEMA IF NOT EXISTS PUBLIC",
-            "CREATE STAGE IF NOT EXISTS STG_RESPIRATORY_SOUNDS DIRECTORY = (ENABLE = TRUE)",
-            """CREATE TABLE IF NOT EXISTS RAW_RESPIRATORY_METADATA (
-                FILE_NAME STRING, DIAGNOSIS STRING, DURATION_SECONDS FLOAT, 
-                SAMPLE_RATE INT, UPLOADED_AT TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
-            )"""
-        ]
-        
-        for cmd in commands:
-            cs.execute(cmd)
-            print(f"✅ Exécuté avec succès")
-            
+        # On lit et exécute le reste du fichier SQL (Tables, Stages)
+        with open('scripts/setup_tessan.sql', 'r') as f:
+            full_sql = f.read()
+            # On sépare par point-virgule pour exécuter commande par commande
+            for cmd in full_sql.split(';'):
+                if cmd.strip():
+                    cur.execute(cmd)
+        print("🚀 Infrastructure déployée avec succès !")
     finally:
-        cs.close()
-        ctx.close()
+        cur.close()
+        conn.close()
 
 if __name__ == "__main__":
-    deploy_infrastructure()
+    deploy()
