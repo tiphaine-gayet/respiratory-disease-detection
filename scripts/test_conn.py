484bca1526ebbb6fe945c11bca37e0a0e509fbbc
Camelia Mazouz <145675762+mica1505@users.noreply.github.com>
2026-03-18 11:38:27 +0100

    fix: wrong file

--- scripts/test_conn.py
+++ scripts/test_conn.py
@@ -2,7 +2,7 @@ import snowflake.connector
 import os
 
 def deploy():
-    # Récupération des variables Git
+    # Récupération des variables configurées sur GitHub
     db = os.getenv('SNOWFLAKE_DATABASE').upper()
     wh = os.getenv('SNOWFLAKE_WAREHOUSE').upper()
 
@@ -15,20 +15,22 @@ def deploy():
 
     try:
         cur = conn.cursor()
-        # On définit le contexte d'abord
+        # Préparation de l'environnement selon les variables Git
         cur.execute(f"CREATE WAREHOUSE IF NOT EXISTS {wh} WITH WAREHOUSE_SIZE='XSMALL'")
         cur.execute(f"USE WAREHOUSE {wh}")
         cur.execute(f"CREATE DATABASE IF NOT EXISTS {db}")
         cur.execute(f"USE DATABASE {db}")
+        cur.execute("CREATE SCHEMA IF NOT EXISTS PUBLIC")
+        cur.execute("USE SCHEMA PUBLIC")
         
-        # On lit et exécute le reste du fichier SQL (Tables, Stages)
+        # Lecture et exécution du fichier de structure
         with open('scripts/setup_tessan.sql', 'r') as f:
-            full_sql = f.read()
-            # On sépare par point-virgule pour exécuter commande par commande
-            for cmd in full_sql.split(';'):
+            sql_commands = f.read().split(';')
+            for cmd in sql_commands:
                 if cmd.strip():
                     cur.execute(cmd)
-        print("🚀 Infrastructure déployée avec succès !")
+        
+        print(f"🚀 Infrastructure Tessan prête dans la base {db} !")
     finally:
         cur.close()
         conn.close()
